1efeaf9eb5331fdc5c46f3bbb6de60e8841de51a
joseluis <jjimgal945a@ieslacampina.es>
2023-05-22 14:42:40 +0200

    Creamos registrar1 y hacemos el html junto al div

---
+++ registrar1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		
+		<title>Club Deportivo La Venta</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<h1>Socios</h1>
+			</div>
