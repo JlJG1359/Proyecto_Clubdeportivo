16d45fa5e0d0d565d577d58bfa1818bbb4a89dda
joseluis <jjimgal945a@ieslacampina.es>
2023-05-22 14:34:03 +0200

    Añadido html, head y script tabla

--- index.php
+++ index.php
@@ -4,3 +4,28 @@ require "conexion.php";
 $sql = "SELECT * FROM clubdeportivo";
 $resultado = $mysqli->query($sql);
 ?>
+
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.4.1.min.js" ></script>
+		<script src="js/bootstrap.min.js" ></script>
+		<script src="js/jquery.dataTables.min.js" ></script>
+		
+		<title>Club Deportivo La Venta</title>
+		
+		<script>
+			$(document).ready( function () {
+    			$(tabla).DataTable();
+			} );
+		</script>
